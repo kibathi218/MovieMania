0819c56893b5afdbeb5a33dd375557b08083964e
kibathi218 <kibathi2180@gmail.com>
2022-10-18 15:01:46 -0400

    finished

--- app.js
+++ app.js
@@ -1,161 +1,69 @@
-fetch("people.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log("error: " + err);
-  });
+var container = document.getElementById("container");
+var movieCardContainer = document.createElement("div");
 
-var netflixContainer = document.getElementById("netflixContainer");
-var huluContainer = document.getElementById("huluContainer");
-var hboMaxContainer = document.getElementById("hboMaxContainer");
-var heroContainer = document.getElementById("hero-container");
-var div = document.createElement("div");
-
-function appendData(data) {
+$.getJSON("data.json", function appendData(data) {
   for (var i = 0; i < data.length; i++) {
-    for (var i = 0; i < data[0].netflix.length; i++) {
+    var platformHeaderContainer = document.createElement("div");
+    platformHeaderContainer.classList.add("platform-header");
+
+    var platformHeader = document.createElement("h2");
+    platformHeader.classList.add("movie-platforms-btn");
+    platformHeader.innerHTML = data[i].platformName;
+
+    platformHeaderContainer.append(platformHeader);
+    container.append(platformHeaderContainer);
+
+    var platformContainer = document.createElement("div");
+    platformContainer.classList.add("platformContainer");
+    container.append(platformContainer);
+
+    for (var j = 0; j < data[i].platformMovies.length; j++) {
+      console.log(data[i].platformMovies[j].movieTitle);
+
+      var likesContainer = document.createElement("div");
+      likesContainer.classList.add("likesContainer");
+
+      var dislikesContainer = document.createElement("div");
+      dislikesContainer.classList.add("dislikesContainer");
 
       var movieCard = document.createElement("div");
       movieCard.classList.add("movieCard");
 
-      var img = document.createElement("img");
-      img.classList.add("movie-img", "netflix");
-      img.src = data[0].netflix[i].imgLink;
+      var movieTitle = document.createElement("h4");
+      movieTitle.innerHTML = data[i].platformMovies[j].movieTitle;
+      var movieImg = document.createElement("img");
+      movieImg.classList.add("movie-img", "netflix");
+      movieImg.src = data[i].platformMovies[j].imgLink;
 
-      var movieTitle = document.createElement("h3");
-      movieTitle.classList.add("movieFont")
-      movieTitle = data[0].netflix[i].movieTitle;
-
-      var likeButton = document.createElement("button")
-      likeButton.classList.add("likeButton")
-      likeButton.innerHTML = "👍"
-      let likes = document.createElement("div")
-      likes.classList.add("hideLikes")
+      var likeButton = document.createElement("button");
+      likeButton.classList.add("likeButton");
+      likeButton.innerHTML = "👍";
+      let likes = document.createElement("div");
+      likes.classList.add("hideLikes");
       let totalLikes = 0;
-      likes.innerHTML = totalLikes
-      let dislikes = document.createElement("div")
+      likes.innerHTML = totalLikes;
+      let dislikes = document.createElement("div");
       let totalDislikes = 0;
-      dislikes.innerHTML = totalDislikes
-      var dislikeButton = document.createElement("button")
-      dislikeButton.classList.add("dislikeButton")
-      dislikeButton.innerHTML = "👎"
+      dislikes.innerHTML = totalDislikes;
+      var dislikeButton = document.createElement("button");
+      dislikeButton.classList.add("dislikeButton");
+      dislikeButton.innerHTML = "👎";
 
-      likeButton.addEventListener("click", function() {
-        totalLikes += 1
-        likes.innerHTML = totalLikes
-        // TO LIKE A MOVIE MORE THAN ONCE
-        // likes.innerHTML = totalLikes
+      likeButton.addEventListener("click", function () {
+        totalLikes += 1;
+        likes.innerHTML = totalLikes;
+      });
 
-      })
+      dislikeButton.addEventListener("click", function () {
+        totalDislikes += 1;
+        dislikes.innerHTML = totalDislikes;
+      });
 
-      dislikeButton.addEventListener("click", function() {
-        totalDislikes += 1
-        dislikes.innerHTML = totalDislikes
+      likesContainer.append(likeButton, likes);
 
-
-      })
-
-      netflixContainer.append(movieCard);
-      movieCard.append(img, movieTitle, likeButton, likes, dislikeButton, dislikes);
+      dislikesContainer.append(dislikeButton, dislikes);
+      movieCard.append(movieImg, movieTitle, likesContainer, dislikesContainer);
+      platformContainer.append(movieCard);
     }
-
-    for (var i = 0; i < data[1].hulu.length; i++) {
-
-      var movieCard = document.createElement("div");
-      movieCard.classList.add("movieCard");
-
-      var img = document.createElement("img");
-      img.classList.add("movie-img", "netflix");
-      img.src = data[1].hulu[i].imgLink;
-      
-      var movieTitle = document.createElement("h3");
-      movieTitle.classList.add("movieFont")
-      movieTitle = data[1].hulu[i].movieTitle;
-
-      var likeButton = document.createElement("button")
-      likeButton.classList.add("likeButton")
-      likeButton.innerHTML = "👍"
-      let likes = document.createElement("div")
-      likes.classList.add("hideLikes")
-      let totalLikes = 0;
-      likes.innerHTML = totalLikes
-      let dislikes = document.createElement("div")
-      let totalDislikes = 0;
-      dislikes.innerHTML = totalDislikes
-      var dislikeButton = document.createElement("button")
-      dislikeButton.classList.add("dislikeButton")
-      dislikeButton.innerHTML = "👎"
-
-      likeButton.addEventListener("click", function() {
-        totalLikes += 1
-        likes.innerHTML = totalLikes
-        // TO LIKE A MOVIE MORE THAN ONCE
-        // likes.innerHTML = totalLikes
-
-      })
-
-      dislikeButton.addEventListener("click", function() {
-        totalDislikes += 1
-        dislikes.innerHTML = totalDislikes
-
-
-      })
-
-      huluContainer.append(movieCard);
-      movieCard.append(img, movieTitle, likeButton, likes, dislikeButton, dislikes);
-    }
-
-
-    for (var i = 0; i < data[2].hboMax.length; i++) {
-
-      var movieCard = document.createElement("div");
-      movieCard.classList.add("movieCard");
-
-
-      var img = document.createElement("img");
-      img.classList.add("movie-img", "netflix");
-      img.src = data[2].hboMax[i].imgLink;
-      
-      var movieTitle = document.createElement("h3");
-      movieTitle.classList.add("movieFont")
-      movieTitle = data[2].hboMax[i].movieTitle;
-
-      var likeButton = document.createElement("button")
-      likeButton.classList.add("likeButton")
-      likeButton.innerHTML = "👍"
-      let likes = document.createElement("div")
-      let totalLikes = 0;
-      likes.innerHTML = totalLikes
-      let dislikes = document.createElement("div")
-      let totalDislikes = 0;
-      dislikes.innerHTML = totalDislikes
-      var dislikeButton = document.createElement("button")
-      dislikeButton.classList.add("dislikeButton")
-      dislikeButton.innerHTML = "👎"
-
-      likeButton.addEventListener("click", function() {
-        totalLikes += 1
-        likes.innerHTML = totalLikes
-        // TO LIKE A MOVIE MORE THAN ONCE
-        // likes.innerHTML = totalLikes
-
-      })
-
-      dislikeButton.addEventListener("click", function() {
-        totalDislikes += 1
-        dislikes.innerHTML = totalDislikes
-
-
-      })
-
-      hboMaxContainer.append(movieCard);
-      movieCard.append(img, movieTitle, likeButton, likes, dislikeButton, dislikes);
-    }
-
-    return;
   }
-}
+});
